21cc515a580fca0f6dd7a526be59825d80f96730
Ayan G. <imluckyayan@gmail.com>
2025-05-20 10:15:14 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# example_chat_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/dev/xkaper/example_chat_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package dev.xkaper.example_chat_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
